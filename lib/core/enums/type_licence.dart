22907fc3c44c70b617397160f67854dd3a6e3440
Rahim10020 <rahialighi@gmail.com>
2025-10-23 16:29:43 +0000

    codage de l'enum type licence

--- lib/core/enums/type_licence.dart
+++ lib/core/enums/type_licence.dart
@@ -1,1 +1,16 @@
+enum TypeLicence {
+  licencePro('Licence Pro'),
+  licenceFond('Licence Fond.'),
+  departement('Département'),
+  parcours('Parcours');
 
+  final String label;
+  const TypeLicence(this.label);
+
+  static TypeLicence fromString(String value) {
+    return TypeLicence.values.firstWhere(
+      (e) => e.name == value || e.label == value,
+      orElse: () => TypeLicence.departement,
+    );
+  }
+}
