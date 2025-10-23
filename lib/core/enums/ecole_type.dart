492e48c53788f18cfb635d5805402cf4a2518fcf
Rahim10020 <rahialighi@gmail.com>
2025-10-23 16:29:00 +0000

    codage de l'enum ecole_type

--- lib/core/enums/ecole_type.dart
+++ lib/core/enums/ecole_type.dart
@@ -1,1 +1,34 @@
+enum EcoleType {
+  ecole,
+  faculte,
+  institut;
 
+  String get label {
+    switch (this) {
+      case EcoleType.ecole:
+        return 'École';
+      case EcoleType.faculte:
+        return 'Faculté';
+      case EcoleType.institut:
+        return 'Institut';
+    }
+  }
+
+  String get icon {
+    switch (this) {
+      case EcoleType.ecole:
+        return '🏫';
+      case EcoleType.faculte:
+        return '🎓';
+      case EcoleType.institut:
+        return '🔬';
+    }
+  }
+
+  static EcoleType fromString(String value) {
+    return EcoleType.values.firstWhere(
+      (e) => e.name == value.toLowerCase(),
+      orElse: () => EcoleType.ecole,
+    );
+  }
+}
