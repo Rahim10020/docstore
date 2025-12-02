29e2940fb9d3498fa26f014be8ec7dcbfe38b473
Rahim10020 <rahialighi@gmail.com>
2025-12-02 16:24:32 +0000

    retour au premier commit

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:docstore/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const DocStoreApp());
+    await tester.pumpWidget(const MyApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
